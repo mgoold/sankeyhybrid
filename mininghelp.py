367889e3379e0334ce8c80fc6e10030391442719
mgoold <markwgoold@gmail.com>
2015-10-12 22:37:36 -0700

    Update mininghelp.py

--- mininghelp.py
+++ mininghelp.py
@@ -1,51 +1,57 @@
 
-# pathkey ['1843', '1843', '1843', '1859', '1859', '1859', '402', '402', '1859', '1843', '1843']
-
 import json
 
-def add(t,path,genorupdate='g',val=0,keynames=None,keyvals=None):
+def add(t,path,genorupdate='g',val=0,brkoval=0,keynames=None,keyvals=None):
 	if genorupdate=='g':
 		for node in path:
 			t = t[node]	
 	elif genorupdate=='i':
-		print 'ininsert', path
+# 		print 'pathininsert', path
 		rowct2=0;
 # 		print 'rowlen', path, len(path)
 		for node in path:
-			print 'rowct2',rowct2, node, val
 			if rowct2==len(path)-1:
-				print 'assigning totals'
+# 				print 'assigning totals'
 				t[node]['total']=val
-				if keynames!=None:
-					for k in keynames:
-						 t[node]['breakouts'][k][keyvals[keynames.index(k)]]=val	
+# 				print 'keynames',keynames, keyvals
+				for k in keynames:
+# 					print 'k is', keyvals[keynames.index(k)]
+					 if keyvals[keynames.index(k)] not in t[node]['breakouts'][k].keys():
+					 	t[node]['breakouts'][k][keyvals[keynames.index(k)]]=brkoval
+					 else:
+						 t[node]['breakouts'][k][keyvals[keynames.index(k)]]+=brkoval	
 			rowct2+=1							 
 			t = t[node]		
-		print 't', json.dumps(t)	
+# 		print 't', json.dumps(t)	
+		
 	elif genorupdate=='u':
-		print 'in update'
-		print 'pathinupdate',path
+# 		print 'in update'
+# 		print 'pathinupdate',path
 		rowct=0;
 		temppath=[]
 		for node in path:
 			temppath.append(node)	
-			print 'rowct in update', rowct		
+# 			print 'rowct in update', rowct, 'node', node
 			if rowct==0:
-				print 'rowct==0'
+# 				print 'rowct==0'
 				tempt=t[node]	
 			else:
-				print 'rowct>0'
+# 				print 'rowct>0'
 				tempt=tempt[node]		
-			print 'temppath',temppath	
-			print 'tempt.get(node)',node, tempt, tempt.get(node)
+# 			print 'temppath',temppath	
+# 			print 'tempt.get(node)',node, tempt, tempt.get(node)
+
+			rowct+=1
 			
 			if tempt.get('total')==None:
-				print 'insertval', keynames,keyvals,type(temppath)
-				add(t,temppath,'i',val,keynames,keyvals)
+# 				print 'nototal', keynames,keyvals,type(temppath)
+				add(t,temppath,'i',val,brkoval,keynames,keyvals)
 			else:
-				print 'updateval'
+# 				print 'havetotal',keynames,keyvals,type(temppath)
 				tempval=tempt['total']
-				print 'tempval total',tempval
+	# 				print 'tempval total',tempval
 				tempval+=1
-				add(t,temppath,'i',tempval,keynames,keyvals) 
-			rowct+=1
+
+				add(t,temppath,'i',tempval,brkoval,keynames,keyvals) 
+				
+
