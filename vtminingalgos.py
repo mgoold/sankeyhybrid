3386677f1643792a42b45d2499d151a83e1e4d6d
mgoold <markwgoold@gmail.com>
2015-10-15 18:27:49 -0700

    Update vtminingalgos.py

--- vtminingalgos.py
+++ vtminingalgos.py
@@ -1,3 +1,5 @@
+import sys
+
 class autoviv(dict):
     """Implementation of perl's autovivification feature."""
     def __getitem__(self, item):
@@ -24,14 +26,7 @@ class autoviv(dict):
 # //	find visits array
 # //	find conversion array
 
-# ma.buildalgoresults(algo='d',evaldict,algoinstr,arraytodistinct,distinctgrain)	
 
-# //needed arguments: 
-# dictionary to build /instructionsdict
-# complete set of pages to eval /evalstr
-# entry page /aid
-# final page /aid
-# intermediatePOIS /aid
 
 def getentrypoint(evalstr,entrypoint):
 	entrypt=evalstr.index(entrypoint)
@@ -68,18 +63,26 @@ def	evalstring(evalstr,instructions=None,evalflg='d',pervalue=None,row=None,subs
 				
 			combinedlist=critindlist+addlpages		
 			
-			strfordict=evalstring(evalstr2[firstentry:len(evalstr2)],None,'o',pervalue,None,combinedlist)
+			strfordict=evalstring(evalstr2[firstentry:len(evalstr2)],instructions,'o',pervalue,None,combinedlist)
 		
 			evaldict=autoviv()
 			evaldict['firstentry']=firstentry
 			evaldict['cleanstring']=evalstr2
 			evaldict['strfordict']=strfordict
-		
-# 		if evalflg=='s':
-# 			endpoint=instdict["algorithm"]["Sandwich"]["endpoint"][pervalue],
-# 			if endpoint!=None and endpoint in evalstr2[firstentry:len(evalstr2)]:
-# 				sandwichpath=[int(i) for i in evalstr2[firstentry:endpoint]]
-# 				evaldict['Sandwich']=sandwichpath
+							
+			if instructions["algorithm"]["type"]=='sandwich':
+				endpoint=instructions["algorithm"]["endpoint"][pervalue]
+# 				print 'endpoint', endpoint, evalstr2[firstentry:len(evalstr2)]
+				if endpoint!=None and endpoint in evalstr2[firstentry:len(evalstr2)]:
+					evalstr3=evalstr2[firstentry:len(evalstr2)]
+					endindex=evalstr3.index(endpoint)+1
+					sandwichpath=[int(i) for i in evalstr3[0:endindex]]
+# 					print 'sandwichpath',sandwichpath
+# 					print 'firstentry', firstentry
+# 					print 'endindex', evalstr2[firstentry:len(evalstr2)].index(endpoint)
+# 					print 'evalstr2', evalstr3
+					
+					evaldict['sandwichpath']=sandwichpath
 		
 		if evalflg=='o':
 			evaldict=[]
@@ -87,30 +90,40 @@ def	evalstring(evalstr,instructions=None,evalflg='d',pervalue=None,row=None,subs
 				if int(i) in substr:
 					evaldict.append(i)					
 		
-## 		print 'evaldict', evaldict
+##  		print 'evaldict', evaldict
 		
 		return evaldict
 
+# algo == type of algorithm to run
+# algodict == dictionary to store results
+# evaldict==dictionary storing data to analyze
+# pervalue == key value from current sql row with which to look up args in instructions dict
 
-# def buildalgoresults(algo='d',pervalue=None,evaldict,algoinstr,arraytodistinct=None,distinctgrain=None):	
-# 	pathkey=','.join(substring)
-# 	
-# 	if algo=='SandPre': 
-# 		d['endpoint']=algoinstr['Sandwich']['endpoint'][pervalue]
-# 		d[pathkey]['entrypage']=evaldict['cleanstring'][0]
-# 		d[pathkey]['length']=len(pathkey)
-# 		entryindex=evaldict['firstentry']
-# 		poi=algoinstr['Sandwich']['poi'][pervalue]
-# 		for k in keynames:
-# 			d[pathkey]['breakouts']=[k]=[keyvals[keynames.index(k)]]
-# 		temppoi=evalstring('o',evaldict['cleanstring'][1:entryindex],poi)
-# 		d[pathkey]['POIs']=','.join(temppoi)
-# 
-# def fillalgovisits(algo='d',evaldict,algodict):	
-# 	
-# 	if algodict=='SandPost': 
-# 		for k in algodict.keys():
+# Path	OK
+# Segment	OK
+# EntryPoint OK
+# EndPoint OK
+# OPS	 OK
+#PathLength	 OK
+
+def buildalgoresults(algodict,evaldict,algoinstr,algo='d',pervalue=None,arraytodistinct=None,distinctgrain=None):	
+	
+	pathkey=','.join(str(i) for i in evaldict['sandwichpath'])
+
+	print 'algo', algo
+	
+	if algo=='sandpre': 
+		algodict['endpoint']=algoinstr['algorithm']['endpoint'][pervalue]
+		algodict[pathkey]['entrypage']=evaldict['cleanstring'][0]
+		algodict[pathkey]['length']=len(evaldict['sandwichpath'])
+		entryindex=evaldict['firstentry']
+		
+		poilist=[i for i in evaldict['cleanstring'] if i in algoinstr['algorithm']['poi'][pervalue]]
 			
+		algodict[pathkey]['poi']=poilist
+		
+		print 'algodict',algodict
+
 			
 		
 		
